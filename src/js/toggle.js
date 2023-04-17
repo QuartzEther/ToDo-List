dea191e430b6a8034078a805d4930acc174f4db3
QuartzEther <quartz.ether@gmail.com>
2023-04-17 21:05:24 +0300

    day&night + create calendar

---
+++ src/js/toggle.js
@@ -0,0 +1,19 @@
+let toggle = document.querySelector('.toggle-switch>input');
+toggle.addEventListener('click', ()=>{
+    if (toggle.checked) {
+        document.querySelector('body').style.backgroundColor = '#434582';
+
+        document.querySelector('.toggle-switch').style.backgroundPosition = "5% 100%";
+        document.querySelector('.toggle-switch').style.boxShadow = "0 0 15px 10px rgba(0,0,0,0.1) inset";
+
+        document.querySelector('.list').classList.add('list_dark-mode');
+    } else {
+        document.querySelector('body').style.backgroundColor = '#F2F8FB';
+
+        document.querySelector('.toggle-switch').style.backgroundPosition = "100% 100%";
+        document.querySelector('.toggle-switch').style.boxShadow = "0 0 15px 10px rgba(0,0,0,0.1) inset";
+
+        document.querySelector('.list').classList.remove('list_dark-mode');
+
+    }
+})
