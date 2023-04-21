4e28a58b27208a5e218020cad43bda0fed0f0893
QuartzEther <quartz.ether@gmail.com>
2023-04-21 23:49:37 +0300

    check/uncheck

--- src/js/app.js
+++ src/js/app.js
@@ -5,19 +5,28 @@ import {Item} from './classes/item.js'
 
 let itemList = [];
 
-new Item('text1', '', false).createItem(itemList);
-new Item('text2', '', false).createItem(itemList);
-new Item('text3', '', true).createItem(itemList);
-
 const todoBlock = document.querySelector('.block_todo');
 const completeBlock = document.querySelector('.block_complete');
 
+const addBtn = document.querySelector('.header__btn');
 
+//--------------------------------------
+
+itemList.push(new Item('text1'));
+itemList.push(new Item('text2'));
+itemList.push(new Item('text3', true));
+
+//create html text
+for (let id in itemList){
+    itemList[id].createHTML(id);
+}
+
+//add items in DOM
 for (let item of itemList){
     if (item.isComplete){
-        completeBlock.insertAdjacentHTML("beforeend", item.createItem(itemList));
+        completeBlock.insertAdjacentHTML("beforeend", item.itemHtml);
     } else {
-        todoBlock.insertAdjacentHTML("beforeend", item.createItem(itemList));
+        todoBlock.insertAdjacentHTML("beforeend", item.itemHtml);
     }
 }
 
@@ -26,3 +35,24 @@ for (let item of document.querySelectorAll('.item')){
 }
 
 //-----------------ADD NEW ITEM----------------
+
+let input = document.querySelector('.header__input');
+
+addBtn.addEventListener('click', ()=>{
+    if (input.value){
+        itemList.push(new Item(input.value));
+
+        todoBlock.insertAdjacentHTML("beforeend", itemList[itemList.length-1].createHTML(itemList.length-1));
+
+        let item = todoBlock.querySelectorAll('.item')[todoBlock.querySelectorAll('.item').length - 1]
+        calendarInit(item.querySelector('.calendar'));
+
+        input.value = '';
+    }
+})
+//----------------Moving Items-------------------
+
+document.querySelector('.checkbox').addEventListener('click', (e) => {
+    let checkbox = e.target.parentNode.parentNode.parentNode;
+    console.log(checkbox);
+})
--- src/js/classes/item.js
+++ src/js/classes/item.js
@@ -1,18 +1,28 @@
 export class Item {
     #itemHTML = '';
-    constructor(text = '', date = '', isComplete = false) {
+    #id = null;
+
+    constructor(text = '', isComplete = false, date = '') {
         this.text = text;
         this.date = date;
         this.isComplete = isComplete;
     }
 
-    createItem(itemList){
-        if (!this.#itemHTML){
+    get itemHtml () {
+        if (this.#itemHTML){
+            return this.#itemHTML;
+        }
+        return null;
+    }
+
+    createHTML(numId = null){
+        if (!this.#id) this.#id = numId;
+        if (this.#id){
             this.#itemHTML = `<div class="block__item item ${this.isComplete ? 'item_complete' : 'item_todo'}">
                 <div class="item__check-text">
                     <div class="item__checkbox checkbox">
-                        <input type="checkbox" ${this.isComplete ? 'checked' : ''} id="checkbox_${itemList.length}" />
-                        <label for="checkbox_${itemList.length}"></label>
+                        <input type="checkbox" ${this.isComplete ? 'checked' : ''} id="checkbox_${this.#id}" />
+                        <label for="checkbox_${this.#id}"></label>
                     </div>
                     <p class="item__text text">${this.isComplete ? `<strike>${this.text}</strike>` : this.text}</p>
                 </div>
@@ -22,8 +32,17 @@ export class Item {
                     <h5 class="calendar__text"></h5>
                 </div>
             </div>`;
-            itemList.push(this);
         }
         return this.#itemHTML
+    }
+
+    toComplete(){
+        this.isComplete = true;
+        return this.createHTML();
+    }
+
+    unComplete(){
+        this.isComplete = false;
+        return this.createHTML();
     }
 }
