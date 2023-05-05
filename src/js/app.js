e6aad3f9861363a85b4c7db94b63dd18af7134c4
QuartzEther <quartz.ether@gmail.com>
2023-05-06 01:00:33 +0300

    Stable desk&phone

--- src/js/app.js
+++ src/js/app.js
@@ -12,31 +12,31 @@ const addBtn = document.querySelector('.header__btn');
 
 //--------------------------------------
 
-itemList.push(new Item('text1text1text1text1text1text1text1text1text1'));
-itemList.push(new Item('text2'));
-itemList.push(new Item('text3', true));
+// itemList.push(new Item('text1'));
+// itemList.push(new Item('text2'));
+// itemList.push(new Item('text3', true));
+//
+// //create html text
+// for (let id in itemList){
+//     itemList[id].createHTML(id);
+// }
+//
+// //add items in DOM
+// for (let item of itemList){
+//     if (item.isComplete){
+//         completeBlock.insertAdjacentHTML("beforeend", item.itemHtml);
+//     } else {
+//         todoBlock.insertAdjacentHTML("beforeend", item.itemHtml);
+//     }
+// }
 
-//create html text
-for (let id in itemList){
-    itemList[id].createHTML(id);
-}
-
-//add items in DOM
-for (let item of itemList){
-    if (item.isComplete){
-        completeBlock.insertAdjacentHTML("beforeend", item.itemHtml);
-    } else {
-        todoBlock.insertAdjacentHTML("beforeend", item.itemHtml);
-    }
-}
-
-//calendar, checkbox and drug&drop init
-for (let item of document.querySelectorAll('.drag-item')){
-    calendarInit(item.querySelector('.calendar'));
-    item.querySelector('.checkbox').addEventListener('click', checkboxTouch)
-    dragItem(item);
-    //item.addEventListener('dblclick', (e)=>{item.style.backgroundColor = 'red'})
-}
+// //calendar, checkbox and drug&drop init
+// for (let item of document.querySelectorAll('.drag-item')){
+//     calendarInit(item.querySelector('.calendar'));
+//     item.querySelector('.checkbox').addEventListener('click', checkboxTouch)
+//     dragItem(item);
+//     //item.addEventListener('dblclick', (e)=>{item.style.backgroundColor = 'red'})
+// }
 
 
 
